323d661037d91e45a1d4b6c8a05dc7a71607f2b9
shiraz <sherazahmed036@gmail.com>
2019-08-18 15:27:31 +0500

    rest_frame viewset added

--- project_restapi/profiles_api/urls.py
+++ project_restapi/profiles_api/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from .views import my_api
+from django.urls import path,include
+from .views import my_api,api_viewsets
+from rest_framework.routers  import DefaultRouter
+
+router=DefaultRouter()
+router.register('api_viewset',api_viewsets,base_name='api_viewset')
+# views.api_viewsets
 
 urlpatterns = [
 
-    path('', my_api.as_view(),name='api'),
+    path('view', my_api.as_view(),name='api'),
+    path('', include(router.urls)),
 ]
--- project_restapi/profiles_api/views.py
+++ project_restapi/profiles_api/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import api_serializer
+from rest_framework import viewsets
+
 # Create your views here.
 class my_api(APIView):
     """Using rest_framework api for crud system"""
@@ -45,3 +47,43 @@ class my_api(APIView):
     def delete(self,request,pk=None):
         """delete an object"""
         return Response({'method used':'DELETE'})
+
+
+class api_viewsets(viewsets.ViewSet):
+    """the above method used is similar to that one with an exception of the listview,updateview,destroy view"""
+    serializer_class = api_serializer
+    def list(self,request):
+        """request a list view"""
+        a_viewset=[
+        'provide more functionality in less code ',
+        'automatically maps to urls using routers',
+        'uses functions like (list,create,retrieve,update,partial_update,destroy)'
+        ]
+
+        return Response({'message':'this is the listview','view':a_viewset
+        })
+
+    def create(self,request):
+        serializer= self.serializer_class(data=request.data) #POST DATA will go to the serializer once we done with that then we can perform new functions
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            email = serializer.validated_data.get('email')
+            message=f'Hellow {name}'
+            return Response({'message':message,'email entered was ':email})
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self,request,pk=None):
+        return Response({'http method':'GET'})
+
+    def update(self,request,pk=None):
+        return Response({'http method':'PUT'})
+
+    def partial_update(self,request,pk=None):
+        return Response({'http method':'patch'})
+
+    def destroy(self,request,pk=None):
+        return Response({'http method':'delete'})
